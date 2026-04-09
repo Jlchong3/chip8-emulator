eefe3835995c01240f7126383d689ab5f5c7fc68
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2026-04-09 14:48:27 -0500

    Create flake.nix for development

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Chip8 Emulator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zig
+
+            wayland
+            libxkbcommon
+            libGL
+
+            alsa-lib
+            libpulseaudio
+
+            libX11
+            libXcursor
+            libXi
+            libXrandr
+          ];
+
+          shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (with pkgs; [
+            wayland
+            libxkbcommon
+            libGL
+            alsa-lib
+            libpulseaudio
+            libX11
+            libXcursor
+            libXi
+            libXrandr
+          ])}:$LD_LIBRARY_PATH"
+        '';
+        };
+      }
+    );
+}
